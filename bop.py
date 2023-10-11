0d5954feed725a1058db33cf28ab92581f4208bd
Jake Duth <jakeduth2@gmail.com>
2023-10-11 01:50:12 -0400

    Test another ifle

---
+++ bop.py
@@ -0,0 +1,23 @@
+def bop():
+    print("bop")
+    return "bop"
+
+
+def head():
+    print("head")
+    return "head"
+
+
+def up():
+    print("up")
+    return "up"
+
+
+def and_(a, b):
+    print("and")
+    return a and b
+
+
+def down():
+    print("down")
+    return "down"
